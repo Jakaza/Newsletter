8d844c9f76ea7f4ef4ce8f58e2874ca312853dc1
Themba G Chauke <goodnessthembac@gmail.com>
2023-06-29 17:30:38 +0200

    newsletter application

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+
+(function () {
+    const mobilePath = "/assets/images/illustration-sign-up-mobile.svg"
+    const desktopPath = "/assets/images/illustration-sign-up-desktop.svg"
+    const imageCon = document.getElementById('sign-up-image-con')
+
+    if (window.innerWidth <= 400) {
+        imageCon.src = mobilePath
+    } else {
+        imageCon.src = desktopPath
+    }
+
+
+
+
+
+
+
+}())
