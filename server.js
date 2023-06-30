8c323e99648ac5af05ebc2c4b33c99225b9c8069
Themba G Chauke <goodnessthembac@gmail.com>
2023-06-30 18:13:57 +0200

    Delete design files.

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const path = require("path")
+const router = require("./route/router")
+const dbConnect = require('./config/dbConnection')
+const main = require('./utils/email')
 require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.sendFile("index.html")
-})
+dbConnect()
+
+main()
+
+
+app.use(express.json())
+app.use('/api/v1', router)
 
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`)
